fc3e1b6a3fa4dfd6e9840cd0ccd99cb79664a4ea
2021baba <89834942+2021baba@users.noreply.github.com>
2024-09-10 23:14:43 +0800

    Add files via upload

---
+++ test18 - 判断列表元素.py
@@ -0,0 +1,13 @@
+print("p" in "python")
+print("k" in "python")
+#判断字母在不在字符串中
+
+lst=[10,20,30,40,50,60,"python"]
+print(10 in lst)
+print(100 in lst)
+print(100 not in lst)
+#in,not in判断
+
+for i in lst:
+    print(i)
+    #依次输出lst中元素
