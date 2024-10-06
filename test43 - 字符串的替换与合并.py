de529b1e7dd777d45e1013474a32d6d7623817e7
2021baba <89834942+2021baba@users.noreply.github.com>
2024-10-06 22:47:30 +0800

    Add files via upload

---
+++ test43 - 字符串的替换与合并.py
@@ -0,0 +1,17 @@
+s = 'hello world'
+print(s.replace('world','Python'))
+s1 = 'hello Python,Python,Python'
+print(s1.replace('Python','Java',2))
+#2表示修改几个
+
+#join连接
+lst = ['hello','Python','Java']
+print('|'.join(lst))
+print(''.join(lst))
+
+t = ('hello','Python','Java')
+print(''.join(t))
+
+print('*'.join('Python'))
+
+
